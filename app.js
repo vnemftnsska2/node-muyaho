90a2fc1ce3ebb58ddff1923f31ebe01180283a9c
tnsska2 <vnemftnsska2@naver.com>
2021-11-29 21:50:37 +0900

    reading test

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.send('Hello Express');
 });
 
+app.get('/reading', (req, res) => {
+    res.json([{id: 1, name: '삼성전자'}, {id: 2, name: '카카오뱅크'}]);
+});
+
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중...'); 
 });
