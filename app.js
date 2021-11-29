dbb681ab6ec1b3aa2780eacd51ac6f0f01efb494
tnsska2 <vnemftnsska2@naver.com>
2021-11-29 22:42:46 +0900

    Test Reading List API

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require("cors");
+
 const app = express();
 app.set('port', process.env.PORT || 5000);
 
@@ -11,6 +13,7 @@ app.get('/reading', (req, res) => {
     res.json([{id: 1, name: '삼성전자'}, {id: 2, name: '카카오뱅크'}]);
 });
 
+app.use(cors({ origin: "http://localhost:3000/" }));
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중...'); 
 });
