80a93a43d951bf1a10c84d987de2ceb2ccc82d07
tnsska2 <vnemftnsska2@naver.com>
2021-11-30 19:43:33 +0900

    modify url

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const app = express();
 app.set('port', process.env.PORT || 5000);
 
 // init test
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     res.send('Hello Express');
 });
 
-app.get('/reading', (req, res) => {
-    res.send([{ id: 1, name: '삼성전자' }, { id: 2, name: '카카오뱅크' }]);
+app.get('/api/reading', (req, res) => {
+    res.send({ aa: 'test' });
 });
 
 
