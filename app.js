308152f48912ddc7dbc3dcd6d920a68c69ee0895
tnsska2 <vnemftnsska2@naver.com>
2021-12-27 00:45:42 +0900

    Leading Stock Update Finish Date

--- app.js
+++ app.js
@@ -172,6 +172,36 @@ app.post('/api/leading/:id', (req, res) => {
     }
 });
 
+app.post('/api/leading/finish/:id', (req, res) => {
+    const stockId = req.params.id;
+    console.log(`FINISH LEADING:: ${stockId}`);
+    const {
+        name,
+        type,
+        bigo,
+    } = req.body;
+    const finishDate = type === 'G' ? req.body.goal_at : req.body.loss_at;
+    
+
+    try {
+        const query = `UPDATE ${process.env.DB_NAME}.leading
+            SET ${type === 'G' ? 'goal_at' : 'loss_at'} = "${moment(finishDate).format('YYYY-MM-DD')}"
+            WHERE id=${req.params.id}`;
+        mariadb.query(query, (err, rows, fields) => {
+            if (!err) {
+                console.log('DELETE SUCCESS');
+                res.send(JSON.stringify({status: 200}));
+            } else {
+                console.log(err);
+                res.send(JSON.stringify({status: 500}));        
+            }
+        });
+    } catch(e) {
+        console.log(e);
+        res.send(JSON.stringify({status: 500}));
+    }
+});
+
 app.post('/api/leading/delete/:id', (req, res) => {
     const stockId = req.params.id;
     console.log(`DELETE LEADING:: ${stockId}`);
